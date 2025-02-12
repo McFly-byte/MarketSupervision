bfdeea96a941cb2fc2127b310c252521f086071a
McFly <24S103360@stu.hit.edu.cn>
2025-02-12 12:12:28 +0800

    init

---
+++ src/main/java/com/group/marketsupervision/MarketSupervisionApplication.java
@@ -0,0 +1,13 @@
+package com.group.marketsupervision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSupervisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketSupervisionApplication.class, args);
+    }
+
+}
